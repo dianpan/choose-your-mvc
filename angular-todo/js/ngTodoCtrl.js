aefe7dddc60c12bfba2bd4738ac281ff7294cbf1
Dian Pan <dian.di.pan@gmail.com>
2016-02-01 15:26:40 -0800

    Add looping and completed states

--- angular-todo/js/ngTodoCtrl.js
+++ angular-todo/js/ngTodoCtrl.js
@@ -1,7 +1,7 @@
-ngTodo.controller('NgTodoCtrl', function($scope) {
+ngTodo.controller('NgTodoCtrl', function ($scope) {
 //angular has dependecy injection, hence the $scope argument
   $scope.todos = [
-    { val: "read a book", completed: false },
+    { val: "bake a cake", completed: false },
     { val: "run a mile", completed: false },
     { val: "take a shower", completed: true },
     { val: "walk the dog", completed: false },
