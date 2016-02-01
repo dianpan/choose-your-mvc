b24c5a1426d236277a303aec204e1c205c83ef98
Dian Pan <dian.di.pan@gmail.com>
2016-02-01 15:15:56 -0800

    add angular to html

--- angular-todo/js/ngTodoCtrl.js
+++ angular-todo/js/ngTodoCtrl.js
@@ -1,4 +1,10 @@
-ngTodo.controller('NgTodoCtrl', function NgTodoCtrl ($scope) {
+ngTodo.controller('NgTodoCtrl', function($scope) {
 //angular has dependecy injection, hence the $scope argument
-
+  $scope.todos = [
+    { val: "read a book", completed: false },
+    { val: "run a mile", completed: false },
+    { val: "take a shower", completed: true },
+    { val: "walk the dog", completed: false },
+    { val: "read a book", completed: true },
+  ];
 });
