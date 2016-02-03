a4af922dacaf56341a867555673a4033c23251d2
Dian Pan <dian.di.pan@gmail.com>
2016-02-03 10:07:09 -0800

    add complete color directive

---
+++ angular-todo/js/directives.js
@@ -0,0 +1,10 @@
+angular.module('sampleDirectives', []).directive('completeColor', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      scope.$watch(attrs.completeColor, function(value) {
+        element.css('color', (value ? 'yellow' : 'limegreen'));
+      });
+    }
+  }
+});
